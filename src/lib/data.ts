d7e83a5e627cdf67eb42e4c39a232e6d11de6416
armi <bahaaeddinebouzid@gmail.com>
2026-09-20 17:07:09 +0100

    dza

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -442,6 +442,6 @@ export const heroPortraitTrio = {
 };
 
 export const aboutPortrait =
-  "open-portfolio\public\images\hero-look-left.jpg";
+  "/images/hero-look-left.jpg";
 
 export const blogUrl = "https://armi64bit.github.io/own-blog/";
